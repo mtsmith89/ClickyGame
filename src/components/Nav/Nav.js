b415c80badfb1beeebc8764069c0593cbed0a22b
Travis Smith <Travis.Smith2@gmail.com>
2018-07-09 18:03:03 -0400

    minor changes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import "./Nav.css";
 const Nav = props => (
   <nav>
     <ul>
-      <li className="brand animated lightSpeedIn">
+      <li className="brand animated infinite jello">
         <a href="/">{props.title}</a>
       </li>
 
